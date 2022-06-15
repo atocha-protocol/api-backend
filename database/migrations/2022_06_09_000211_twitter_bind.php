c485c13d1c3c7c2e5d09771a36f15b1b7ad6ce08
h12 <630086711@qq.com>
2022-06-15 09:10:25 +0800

    Add migrate file of twitter_bind.

---
+++ database/migrations/2022_06_09_000211_twitter_bind.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twitter_bind', function (Blueprint $table) {
+            $table->increments('id'); //
+            $table->string('ato_address')->unique();
+            $table->string('twitter_screen_name')->nullable()->unique();
+            $table->string('twitter_profile_image_url')->nullable();
+            $table->string('twitter_profile_image_url_https')->nullable();
+            $table->string('oauth_token')->nullable();;
+            $table->string('oauth_token_secret')->nullable();;
+            $table->string('twitter_full_data')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // NONE
+    }
+};
